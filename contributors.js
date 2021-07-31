ae13a38114e91942b2af423c98890900bc817bd7
adnan-sabbir <adnan.sabbir@g.bracu.ac.bd>
2021-07-31 10:57:28 +0600

    Removed dummy push

--- contributors.js
+++ contributors.js
@@ -9,13 +9,6 @@ const contributors = [
 		linkedinProfile: 'https://www.linkedin.com/in/adnansabbir/',
 		githubProfile: 'https://github.com/adnansabbir',
 	},
-	{
-		name: 'Floating Ghost 2',
-		description: 'A simple floating ghost animation for your homepage 2',
-		previewImageLink: 'https://res.cloudinary.com/adnansabbir/image/upload/v1624271784/floating-ghost_sehaa8.gif',
-		author: 'Rakib',
-		folderName: 'rakib'
-	},
 	// Add your contributor info above this line and don't remove these line. 
 	// Make sure you put a comma after your second bracket.
 ]
