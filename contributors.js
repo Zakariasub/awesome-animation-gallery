ea9ee525d072a8f096051f02a34f603e21f325c4
Zakariasub <md.zakariasub@gmail.com>
2023-06-02 23:07:50 +0600

    added my ghost animation

--- contributors.js
+++ contributors.js
@@ -9,6 +9,16 @@ const contributors = [
 		linkedinProfile: 'https://www.linkedin.com/in/adnansabbir/',
 		githubProfile: 'https://github.com/adnansabbir',
 	},
+	{
+		name: 'Floating Ghost v2',
+		description: 'A simple floating ghost animation for your homepage',
+		previewImageLink: 'https://res.cloudinary.com/dnfrabidt/image/upload/v1685725083/ezgif-5-3cdc8a596e_yoo0g3.gif',
+		author: 'Zakaria',
+		folderName: 'Zakaria',
+		facebookProfile: 'https://www.facebook.com/profile.php?id=100010773615973',
+		linkedinProfile: 'https://www.linkedin.com/in/zakaria-hossain-995a99176/',
+		githubProfile: 'https://github.com/Zakariasub',
+	},
 	// Add your contributor info above this line and don't remove these line. 
 	// Make sure you put a comma after your second bracket.
 ]
